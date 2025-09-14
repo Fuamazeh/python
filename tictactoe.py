f39f4b5397ad5a5a2b1456bc8a301c2d456f697a
ACHA GEORGE <achageorge@icloud.com>
2025-09-14 05:16:29 +0100

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,110 @@
+#tictactoe
+import random
+board = ['-', '-', '-',
+        '-', '-', '-',
+        '-', '-', '-']
+currentPlayer = 'X'
+winner = None
+gameRunning = True
+
+#printing the game board
+def printBoard(board):
+    print(f'''
+    {board[0]} | {board[1]} | {board[2]}
+    ---------
+    {board[3]} | {board[4]} | {board[5]}
+    ---------
+    {board[6]} | {board[7]} | {board[8]}
+    ''')
+
+#player input
+def playerInput(board):
+    inp = int(input('Enter position number 1-9: '))
+    if inp >= 1 and inp <= 9 and board[inp-1] == '-':
+        board[inp-1] = currentPlayer
+    elif inp < 1 or inp > 9:
+        print('Number is out of range!')
+        playerInput(board)
+    else:
+        print('Oops player is already in that spot!')
+        playerInput(board)
+
+#check win or tie
+def checkHorizontal(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[0] != '-':
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != '-':
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != '-':
+        winner = board[6]
+        return True
+        
+def checkVertical(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != '-':
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != '-':
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != '-':
+        winner = board[2]
+        return True
+        
+def checkDiag(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != '-':
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != '-':
+        winner = board[2]
+        return True
+
+def checkWin(board):
+    global gameRunning
+    if checkDiag(board) or checkVertical(board) or checkHorizontal(board):
+        gameRunning = False
+        if winner == 'X':
+            printBoard(board)
+            print('You Win!')
+        else:
+            printBoard(board)
+            print('The Computer Wins!')
+            
+def checkTie(board):
+    global gameRunning
+    if '-' not in board:
+        gameRunning = False
+        printBoard(board)
+        print('DRAW!')
+        gameRunning = False
+
+
+#switching player
+def switchPlayer():
+    global currentPlayer
+    if currentPlayer == 'X':
+        currentPlayer = 'O'
+    else:
+        currentPlayer = 'X'
+        
+#Computer
+def computer(board):
+    while currentPlayer == 'O':
+        position = random.randint(0, 8)
+        if board[position] == '-':
+            board[position] = 'O'
+            switchPlayer()
+
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
+    checkWin(board)
+    checkTie(board)
+    switchPlayer()
+    computer(board)
+    checkWin(board)
+    checkTie(board)
